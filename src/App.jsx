fc9db69b1c19c4961c7f1d00cfaa3188ae5192e7
Rahul-max55 <birlarahul340@gmail.com>
2023-08-15 23:40:37 +0530

    new updates in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React , {useState} from "react";
+import React, {useState } from "react";
+import { Link } from "react-router-dom";
 import cover from "./Assets/fullscreendark.jpg";
 import { CgProfile } from "react-icons/cg";
 import { AiFillProfile } from "react-icons/ai";
@@ -12,14 +13,12 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import Typewriter from "typewriter-effect";
 
 const App = () => {
-  const [hideNav, sethideNav] = useState(false);
+  const [hideNav, sethideNav] = useState(true);
 
-  const handleClose = (e) =>{
-        e.preventDefault();
-        sethideNav(!hideNav)
-  }
-
-  console.log(hideNav);
+  const handleClose = (e) => {
+    e.preventDefault();
+    sethideNav(!hideNav);
+  };
 
   return (
     <>
@@ -31,14 +30,44 @@ const App = () => {
         }}
       >
         <div className="ml-10">
-          <div className={`bg-gray-600 w-20 rounded-full flex justify-evenly items-center overflow-hidden flex-col ${hideNav ? "h-20" : "h-96"}`}>
-            <div className={`bg-gray-600 w-20 h-24 rounded-full flex justify-evenly text-3xl text-gray-300 items-center flex-col `} onClick={handleClose} ><GiHamburgerMenu /></div>
-            <div className={`bg-gray-600 w-20  overflow-hidden rounded-full flex justify-between text-2xl items-center text-gray-300 flex-col ${hideNav ? "h-0" : "h-96 pb-8"}`}>
-            <span className="relative" ><FaHome /> <span className="absolute top-0 left-16 text-xl" >HOME</span> </span>
-             <span className="relative" ><BsFillPersonFill /> <span className="absolute top-0 left-16 text-xl" >ABOUT</span> </span>
-             <span className="relative" ><BiSolidBriefcase /> <span className="absolute top-0 left-16 text-xl" >PORTFOLIO</span> </span>
-             <span className="relative" ><HiMail /> <span className="absolute top-0 left-16 text-xl" >CONTACT</span> </span>
-             <span className="relative" ><ImBlogger /> <span className="absolute top-0 left-16 text-xl" >BLOG</span> </span>
+          <div
+            className={`bg-gray-600 w-20 rounded-full transition-all duration-500 ease-linear flex justify-evenly items-center  flex-col ${
+              hideNav ? "h-20 overflow-hidden" : "h-96"
+            }`}
+          >
+            <div
+              className={`bg-gray-600 w-20 h-24 rounded-full hover:text-gray-100 cursor-pointer flex justify-evenly text-3xl text-gray-300 items-center flex-col `}
+              onClick={handleClose}
+            >
+              <GiHamburgerMenu />
+            </div>
+            <div
+              className={`bg-gray-600 w-20 rounded-full flex transition-all duration-500 ease-linear  justify-between text-2xl items-center text-gray-300 flex-col ${
+                hideNav ? "h-0 overflow-hidden" : "h-96 pb-8"
+              }`}
+            >
+              <Link to="/" className="relative hover:text-yellow-400  focus:text-yellow-400 cursor-pointer">
+                <FaHome />
+                <span className="absolute top-0 left-16 text-xl">HOME</span>
+              </Link>
+              <Link to="/about" className="relative hover:text-yellow-400  focus:text-yellow-400 cursor-pointer">
+                <BsFillPersonFill />
+                <span className="absolute top-0 left-16 text-xl">ABOUT</span>
+              </Link>
+              <Link to="/portfolio" className="relative hover:text-yellow-400  focus:text-yellow-400 cursor-pointer">
+                <BiSolidBriefcase />
+                <span className="absolute top-0 left-16 text-xl">
+                  PORTFOLIO
+                </span>
+              </Link>
+              <Link to="/contact" className="relative hover:text-yellow-400  focus:text-yellow-400 cursor-pointer">
+                <HiMail />
+                <span className="absolute top-0 left-16 text-xl">CONTACT</span>
+              </Link>
+              <Link to="/blog" className="relative hover:text-yellow-400  focus:text-yellow-400 cursor-pointer">
+                <ImBlogger />
+                <span className="absolute top-0 left-16 text-xl">BLOG</span>
+              </Link>
             </div>
           </div>
           {/* <div className="bg-purple-500 w-20 h-72">lorem</div> */}
@@ -65,13 +94,13 @@ const App = () => {
               aspernatur debitis repudiandae nostrum aperiam eum quam.
             </p>
             <div className="flex space-x-4">
-              <button className="flex px-5 py-3 items-center justify-center bg-yellow-400 text-xl font-medium text-white space-x-2 rounded-full shadow-lg border-2 border-yellow-400">
+              <Link to="/about" className="flex px-5 py-3 items-center justify-center bg-yellow-500 transition-colors text-xl font-medium text-white space-x-2 rounded-full shadow-lg border-2 border-yellow-400 hover:bg-transparent">
                 <CgProfile /> <span>MORE ABOUT ME </span>
-              </button>
-              <button className="flex px-5 py-3 items-center justify-center border-2 border-yellow-400 text-xl font-medium text-white space-x-2 rounded-full shadow-lg">
+              </Link>
+              <Link to="/portfolio" className="flex px-5 py-3 items-center justify-center border-2 border-yellow-400 text-xl font-medium text-white space-x-2 rounded-full shadow-lg hover:bg-yellow-500">
                 <AiFillProfile />
                 <span>PORTFOLIO</span>
-              </button>
+              </Link>
             </div>
           </div>
           <div className="bg-gray-300 w-16 h-16  rounded-full flex justify-center text-4xl items-center ">
